c498fda8275f2a49f57daa92f053e0d3e32d8f39
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-10 13:16:30 +0200

    create user controller

---
+++ backend/controllers/user.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const usermodel = require ('../database/models/userSchema')
+
+const createNewUser = (req,res)=>{
+    const {firstName,lastName,age,email,password}=req.body;
+    const newUser = new usermodel ( {firstName,lastName,age,email,password})
+    newUser.save().then((result)=>{
+        res.status(201)
+        res.json({success:true, result:result})
+    }).catch((err)=>{
+        res.status(500);
+        res.json(err);
+    })
+}
+module.exports= {
+    createNewUser
+}
