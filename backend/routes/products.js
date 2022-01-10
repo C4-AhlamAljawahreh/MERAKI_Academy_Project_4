b902a0d39f3f007a4fcf1c851a2ebfdf2f4e3d9e
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-10 20:14:49 +0200

    delete product by id route

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {createNewProduct , updateProductById} = require('../controllers/products')
+const {createNewProduct , updateProductById,deleteProductById} = require('../controllers/products')
 const {authentication } = require('../middleware/authentication')
 
 const productRoute = express.Router();
@@ -8,6 +8,8 @@ const productRoute = express.Router();
 //      /product
 productRoute.post('/',authentication,createNewProduct);
 productRoute.put('/:id',authentication,updateProductById);
+productRoute.delete('/:id',authentication,deleteProductById);
+
 
 
 module.exports = productRoute;
