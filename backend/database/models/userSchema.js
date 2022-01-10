8178508dfc3dffb36ae4fc024e53bed0530aa368
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-10 13:19:19 +0200

    edit userSchema

--- backend/database/models/userSchema.js
+++ backend/database/models/userSchema.js
@@ -5,7 +5,10 @@ firstName:{type:String,required:true},
 lastName:{type:String,required:true},
 age:{type:Number},
 email:{type:String,required:true,unique:true},
+password:{type:String,required:true},
 role: {type:mongoose.Schema.Types.ObjectId, ref:"Role"}
 })
+
+
 
 module.exports = mongoose.model('User',userSchema)
