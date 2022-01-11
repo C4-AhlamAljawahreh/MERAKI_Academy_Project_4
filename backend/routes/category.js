a6cf0ab6aa80ec6f58cc65cab0fe6d6bbf9d052d
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-11 18:30:25 +0200

    create category router

---
+++ backend/routes/category.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const {createNewCategory} = require('../controllers/category')
+
+const categoryRoute = express.Router();
+
+
+//         /category
+categoryRoute.post('/',createNewCategory);
+
+module.exports = categoryRoute;
