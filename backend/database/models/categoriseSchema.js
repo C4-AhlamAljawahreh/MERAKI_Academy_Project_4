fd18f88cc3a60cf0c79998fb8b6948f7c3a537f0
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-11 13:15:04 +0200

    create categories schema

---
+++ backend/database/models/categoriseSchema.js
@@ -0,0 +1,7 @@
+const mongoose = require ('mongoose')
+
+const categoriesSchema = new mongoose.Schema({
+name:{type:'String',required:true,unique:true}
+})
+
+module.exports = mongoose.model('Categories',categoriesSchema)
