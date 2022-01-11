b07e9d9e50a2b3b5e54a94065c534cb6cf6032a9
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-11 12:59:33 +0200

    get product by Id route

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {createNewProduct , updateProductById,deleteProductById,getAllProducts} = require('../controllers/products')
+const {createNewProduct , updateProductById,deleteProductById,getAllProducts,getProductById} = require('../controllers/products')
 const {authentication } = require('../middleware/authentication')
 const {authorization} = require('../middleware/authorization')
 
@@ -8,6 +8,7 @@ const productRoute = express.Router();
 
 //      /product
 productRoute.get('/',getAllProducts)
+productRoute.get('/:id',getProductById)
 productRoute.post('/',authentication,authorization('ADD_PRODUCT'),createNewProduct);
 productRoute.put('/:id',authentication,authorization('UPDATE_PRODUCT'),updateProductById);
 productRoute.delete('/:id',authentication,authorization('DELETE_PRODUCT'),deleteProductById);
