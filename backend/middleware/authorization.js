6f7a087baeb61167e906182fd6b91e7707d672c2
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-11 11:41:22 +0200

    authorization

---
+++ backend/middleware/authorization.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const authorization =(string)=>{
+    return (req,res,next)=>{
+        const token = req.headers.authorization.split(' ').pop();
+     const verified = jwt.verify(token,process.env.SECRET);
+     console.log()
+     const foundPermission = verified.role.permissions.find((element)=>{
+         return element == string;
+     }
+     )
+if(foundPermission){
+    next();
+}else {
+    res.status(401)
+    res.json({success:"false", message:"Unauthorize"})
+}
+    }
+}
+module.exports={authorization}
