170be28806ba93f1c30a22c5a3bb9b79c79883d7
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-11 18:34:41 +0200

    edit category files name

--- backend/database/models/categoriseSchema.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require ('mongoose')
-
-const categoriesSchema = new mongoose.Schema({
-name:{type:'String',required:true,unique:true}
-})
-
-module.exports = mongoose.model('Categories',categoriesSchema)
