a3f01a8d6b903070e98730ad1a98d0c0186dbebb
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-11 11:42:33 +0200

    edit product to check authorization for each method

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const {createNewProduct , updateProductById,deleteProductById} = require('../controllers/products')
 const {authentication } = require('../middleware/authentication')
+const {authorization} = require('../middleware/authorization')
 
 const productRoute = express.Router();
 
 
 //      /product
-productRoute.post('/',authentication,createNewProduct);
-productRoute.put('/:id',authentication,updateProductById);
-productRoute.delete('/:id',authentication,deleteProductById);
+productRoute.post('/',authentication,authorization('ADD_PRODUCT'),createNewProduct);
+productRoute.put('/:id',authentication,authorization('UPDATE_PRODUCT'),updateProductById);
+productRoute.delete('/:id',authentication,authorization('DELETE_PRODUCT'),deleteProductById);
 
 
 
