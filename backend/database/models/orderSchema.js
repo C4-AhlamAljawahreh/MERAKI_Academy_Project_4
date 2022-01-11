1989d1c6b58adc36755eb124a9dcb9170c06b442
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-11 17:15:02 +0200

    edit order schema

--- backend/database/models/orderSchema.js
+++ backend/database/models/orderSchema.js
@@ -2,7 +2,7 @@ const mongoose = require ('mongoose')
 
 const orderSchema = new mongoose.Schema({
 products:{type:[mongoose.Schema.Types.ObjectId],ref:'Product'},
-totalPrice:{type:[Number]},
+totalPrice:{type:Number},
 userId:{type:mongoose.Schema.Types.ObjectId,ref:'User'}
 })
 
