ce6f2ea7b7046ae0d57e5677309d67759362fe78
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-11 11:43:34 +0200

    edit login to accept role

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -6,12 +6,13 @@ const logIn = (req, res) => {
   const { email, password } = req.body;
   userModel
     .findOne({ email: email })
-    .populate("role")
+    .populate("role","-_id -__v")
     .then(async (result) => {
       const varified = await bcrypt.compare(password, result.password);
       if (!varified) {
         res.json({ message: "you entered wrong Password" });
       } else {
+        console.log(result)
         const payload = {
           userId: result._id,
           username: result.firstName,
