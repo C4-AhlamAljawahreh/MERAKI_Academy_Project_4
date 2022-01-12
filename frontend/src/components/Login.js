b1a98443c88439bbbc734e8c0ab538bae610f297
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-12 13:37:47 +0200

    Login component Done

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,27 @@
+import React ,{useState} from 'react'
+import axios from 'axios'
+
+
+const Login =()=>{
+const[email,setEmail]=useState(' ')
+const[password,setPassword]=useState(' ')
+    return(<>
+    <div className='Login'>
+        <input placeholder='email' type='email' onChange={(e)=>{
+            setEmail(e.target.value)
+
+        }}/>
+        <input placeholder='password' type='password' onChange={(e)=>{
+            setPassword(e.target.value)
+        }}/>
+        <button onClick={()=>{
+            axios.post('http://localhost:5000/login',{email,password}).then((result)=>{
+                console.log(result.data)
+            }).catch((err)=>{
+                console.log('error')
+            })
+        }}>Login</button>
+    </div>
+    </>)
+}
+export default Login;
