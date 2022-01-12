a3cfe014371fc3d0380b36eda183d967bda0d1ee
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-12 13:40:18 +0200

    create navigation component & add link route routes temporarily

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,36 @@
 import "./App.css";
+import Login from './components/Login'
+import { Routes, Route, Link, Router } from "react-router-dom";
+
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome To App</h1>
+      <Navigation/>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Login/>}/>
+
+      </Routes>
+      {/* <Login/> */}
     </div>
   );
+}
+
+
+const Navigation =()=>{
+  return(
+    <>
+    <div className="Navigation" style={{display:"flex",gap:"20px"}}>
+      <Link to='/login'>login   </Link>
+      <Link to='/register'>register</Link>
+
+      </div>
+      </>
+  )
 }
 
 export default App;
