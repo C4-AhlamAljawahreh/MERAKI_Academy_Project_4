9d2cd5bd3b1d3c75e509cd0402bdb0584e5f6974
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-12 11:55:55 +0200

    get order by id route

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const {createOrder,getAllOrders,deleteOrderById} = require ('../controllers/order')
+const {createOrder,getAllOrders,deleteOrderById,getOrderById} = require ('../controllers/order')
 
 
 const orderRoute = express.Router();
 
 orderRoute.post('/',createOrder)
-orderRoute.get('/',getAllOrders)
-orderRoute.delete('/:id',deleteOrderById)
-
+orderRoute.get('/',getAllOrders)// dont forget to add authorization to admin
+orderRoute.delete('/:id',deleteOrderById)// dont forget to add authorization to admin
+orderRoute.get('/:id',getOrderById)
 
 
 module.exports = orderRoute;
