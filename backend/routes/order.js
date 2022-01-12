10a261bec83ca28e7f3df3ae1da06a6255296bb8
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-12 11:48:00 +0200

    delete order by id

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const {createOrder,getAllOrders} = require ('../controllers/order')
+const {createOrder,getAllOrders,deleteOrderById} = require ('../controllers/order')
 
 
 const orderRoute = express.Router();
 
 orderRoute.post('/',createOrder)
 orderRoute.get('/',getAllOrders)
+orderRoute.delete('/:id',deleteOrderById)
 
 
 
