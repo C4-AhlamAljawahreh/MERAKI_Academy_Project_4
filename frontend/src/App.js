7ea024b2aa9893662e1654abfc213020d58c253c
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-12 15:26:44 +0200

    pass props "islogin" to hide and show login register link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,19 @@ import "./App.css";
 import Login from "./components/Login";
 import { Routes, Route, Link, Router } from "react-router-dom";
 import Register from './components/Rigester'
+import { useState } from "react";
 
 function App() {
+  const [token,setToken]=useState('')
+  const [isLogin,setIsLogin]=useState(false)
+
+
   return (
     <div className="App">
       <h1>Welcome To App</h1>
-      <Navigation />
+      <Navigation isLogin={isLogin}/>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setToken={setToken} setIsLogin={setIsLogin} />} />
         <Route path="/register" element={<Register />} />
       </Routes>
       {/* <Login/> */}
@@ -17,12 +22,13 @@ function App() {
   );
 }
 
-const Navigation = () => {
+const Navigation = (props) => {
   return (
     <>
       <div className="Navigation" style={{ display: "flex", gap: "20px" }}>
-        <Link to="/login">login </Link>
-        <Link to="/register">register</Link>
+      {!props.isLogin?<><Link to="/login">login </Link>
+        <Link to="/register">register</Link></>:<></>}
+
       </div>
     </>
   );
