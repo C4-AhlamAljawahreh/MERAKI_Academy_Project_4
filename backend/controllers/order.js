ab084d3dd9c52740e059d5f1e8ff19d12d2885f5
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-12 11:37:28 +0200

    get all orders

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -1,24 +1,23 @@
 const orderModel = require ('../database/models/orderSchema')
-const productModel = require ('../database/models/productSchema')
 
 
 
-const createOrder = async (req,res)=>{
-const {products,userId}=req.body;
+const createOrder = (req,res)=>{
+const {products,totalPrice,userId}=req.body;
 // let totalPrice =0;
-const total= await products.map((element,index)=>{
-    productModel.findOne({_id: element}).then((result)=>{
-    console.log(result.price);
-    return result.price
-    })
-})
-console.log(total)
-// console.log(totalPrice)
-const newOrder = new orderModel({products,userId})
+const newOrder = new orderModel({products,totalPrice,userId})
 newOrder.save().then((order)=>{
-    res.json({order:order})
+    res.json({success:"true",message:'successfully created Order',order:order})
 }).catch((err)=>{
-res.json('error')})
+res.json({success:"false",message:'failed to create Order'})})
 }
-
-module.exports={createOrder}
+const getAllOrders = (req,res)=>{
+    orderModel.find({}).then((result)=>{
+        res.status(200)
+        res.json({success:'true',message:'all orders:' ,orders: result})
+    }).catch((err)=>{
+        res.status(500)
+        res.json({success:'false',message:'failed to get all orders' })
+    })
+}
+module.exports={createOrder , getAllOrders}
