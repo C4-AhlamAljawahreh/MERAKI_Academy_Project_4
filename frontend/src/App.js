a617f5ab6c04b1a43dd3de096c499146b9e8150f
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-12 15:14:10 +0200

    edit format for document

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,31 @@
 import "./App.css";
-import Login from './components/Login'
+import Login from "./components/Login";
 import { Routes, Route, Link, Router } from "react-router-dom";
-
-
-
+import Register from './components/Rigester'
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome To App</h1>
-      <Navigation/>
+      <Navigation />
       <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Login/>}/>
-
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
       {/* <Login/> */}
     </div>
   );
 }
 
-
-const Navigation =()=>{
-  return(
+const Navigation = () => {
+  return (
     <>
-    <div className="Navigation" style={{display:"flex",gap:"20px"}}>
-      <Link to='/login'>login   </Link>
-      <Link to='/register'>register</Link>
-
+      <div className="Navigation" style={{ display: "flex", gap: "20px" }}>
+        <Link to="/login">login </Link>
+        <Link to="/register">register</Link>
       </div>
-      </>
-  )
-}
+    </>
+  );
+};
 
 export default App;
