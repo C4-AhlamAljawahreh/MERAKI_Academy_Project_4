0f2227b0fd3a19c608f24f912524454a8874f85b
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-13 12:56:30 +0200

    create NewProduct component

---
+++ frontend/src/components/NewProduct.js
@@ -0,0 +1,58 @@
+import React, { useState, useContext, useEffect } from "react";
+import { TokenContext } from "../App";
+import axios from "axios";
+const NewProduct = () => {
+  const token = useContext(TokenContext);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [image, setImage] = useState("");
+
+  return (
+    <>
+      <div className="NewProduct" style={{ display: "grid", gap: "20px" }}>
+        New product
+        <input
+          placeholder="product name"
+          type="text"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <input
+          placeholder="product price"
+          type="text"
+          onChange={(e) => {
+            setPrice(e.target.value);
+          }}
+        />
+        <input
+          placeholder="image url "
+          type="text"
+          onChange={(e) => {
+            setImage(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            axios
+              .post(
+                "http://localhost:5000/product",
+                { name, price, image },
+                {
+                  headers: {
+                    authorization: "Bearer " + token,
+                  },
+                }
+              )
+              .then((response) => {
+                console.log(response.data);
+              });
+          }}
+        >
+          add product to market
+        </button>
+      </div>
+    </>
+  );
+};
+export default NewProduct;
