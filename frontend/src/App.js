4fcf74d0b5cb716a14e29f0548ff37405322a812
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-13 13:00:23 +0200

    useContext , add market and new product route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,34 @@
 import "./App.css";
 import Login from "./components/Login";
-import { Routes, Route, Link, Router } from "react-router-dom";
-import Register from './components/Rigester'
-import { useState } from "react";
+import { Routes, Route, Link} from "react-router-dom";
+import Register from "./components/Rigester";
+import Market from "./components/Market";
+import NewProduct from "./components/NewProduct";
+import { useState , createContext } from "react";
 
+
+export const TokenContext = createContext();
 function App() {
-  const [token,setToken]=useState('')
-  const [isLogin,setIsLogin]=useState(false)
-
+  const [token, setToken] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
+   
 
   return (
     <div className="App">
       <h1>Welcome To App</h1>
-      <Navigation isLogin={isLogin}/>
+      <Navigation isLogin={isLogin} />
+      {/* <TokenContext.Provider value={token}>
+        {isLogin?<Market/>:<></>}
+          </TokenContext.Provider> */}
       <Routes>
-        <Route path="/login" element={<Login setToken={setToken} setIsLogin={setIsLogin} />} />
+        <Route
+          path="/login"
+          element={!isLogin? <Login setToken={setToken} setIsLogin={setIsLogin} />:<></>}
+        />
         <Route path="/register" element={<Register />} />
+        <Route path="/market" element={isLogin?<TokenContext.Provider value={token}><Market/></TokenContext.Provider>:<></>} />
+        <Route path="/newProduct" element={isLogin?<TokenContext.Provider value={token}><NewProduct/></TokenContext.Provider>:<></>} />
+
       </Routes>
       {/* <Login/> */}
     </div>
@@ -26,9 +39,15 @@ const Navigation = (props) => {
   return (
     <>
       <div className="Navigation" style={{ display: "flex", gap: "20px" }}>
-      {!props.isLogin?<><Link to="/login">login </Link>
-        <Link to="/register">register</Link></>:<></>}
-
+        {!props.isLogin ? (
+          <>
+            <Link to="/login">login </Link>
+            <Link to="/register">register</Link>
+          </>
+        ) : (
+          <><Link to="/market">Market</Link>
+          <Link to="/newProduct">NewProduct</Link></>
+        )}
       </div>
     </>
   );
