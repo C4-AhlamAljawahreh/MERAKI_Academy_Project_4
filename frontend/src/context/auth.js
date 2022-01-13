318d99440206d658079a4e7a118f9b77111ec16a
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-14 00:09:18 +0200

    useState (role,cart,username)

--- frontend/src/context/auth.js
+++ frontend/src/context/auth.js
@@ -1,31 +1,28 @@
-import React, { useState,createContext ,useEffect} from "react";
+import React, { useState, createContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
-
-
 
 const LoginProvider = (props) => {
   const goTo = useNavigate();
 
   const [isLogIn, setIsLogIn] = useState(false);
   const [token, setToken] = useState("");
+  const [username, setUsername] = useState("");
+  const [role, setRole] = useState("");
+  const [cart, setCart] = useState([]);
 
+  //to save user process .
+  useEffect(() => {
+    saveToken(localStorage.getItem("token"));
+  }, []);
 
-//to save user process .
-useEffect(()=>{
-    saveToken(localStorage.getItem('token'))
-},[]) 
-
-
-//this function to save the token 
+  //this function to save the token
   const saveToken = (token) => {
     setToken(token);
     setIsLogIn(true);
     localStorage.setItem("token", token);
   };
-
-
 
   const logout = () => {
     setIsLogIn(false);
@@ -33,16 +30,19 @@ useEffect(()=>{
     goTo("/login");
   };
 
-
-
   const state = {
     token,
     isLogIn,
+    username,
+    role,
     logout,
     saveToken,
     setIsLogIn,
+    setUsername,
+    setRole,
+    cart,
+    setCart,
   };
-
 
   return (
     <AuthContext.Provider value={state}>{props.children}</AuthContext.Provider>
