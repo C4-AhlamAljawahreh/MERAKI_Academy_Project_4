0a602198dd597245c46b8f76a7bdc3bf5a708063
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-13 18:01:41 +0200

    activate AuthContext & remove Navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,33 @@
 import "./App.css";
-import Login from "./components/Login";
-import { Routes, Route, Link} from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/auth";
+
+
+import Navigation from "./components/Navigation";
+import Login from "./components/Login"
 import Register from "./components/Rigester";
 import Market from "./components/Market";
 import NewProduct from "./components/NewProduct";
-import { useState , createContext } from "react";
 
-
-export const TokenContext = createContext();
 function App() {
-  const [token, setToken] = useState("");
-  const [isLogin, setIsLogin] = useState(false);
-   
+  const {isLogIn } = useContext(AuthContext);
 
   return (
     <div className="App">
       <h1>Welcome To App</h1>
-      <Navigation isLogin={isLogin} />
-      {/* <TokenContext.Provider value={token}>
-        {isLogin?<Market/>:<></>}
-          </TokenContext.Provider> */}
-      <Routes>
-        <Route
-          path="/login"
-          element={!isLogin? <Login setToken={setToken} setIsLogin={setIsLogin} />:<></>}
-        />
-        <Route path="/register" element={<Register />} />
-        <Route path="/market" element={isLogin?<TokenContext.Provider value={token}><Market/></TokenContext.Provider>:<></>} />
-        <Route path="/newProduct" element={isLogin?<TokenContext.Provider value={token}><NewProduct/></TokenContext.Provider>:<></>} />
+      <Navigation />
 
+      <Routes>
+        <Route path="/login" element={!isLogIn ? <Login /> : <></>} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/market" element={isLogIn ? <Market /> : <></>} />
+        <Route path="/newProduct" element={isLogIn ? <NewProduct /> : <></>} />
       </Routes>
-      {/* <Login/> */}
     </div>
   );
 }
 
-const Navigation = (props) => {
-  return (
-    <>
-      <div className="Navigation" style={{ display: "flex", gap: "20px" }}>
-        {!props.isLogin ? (
-          <>
-            <Link to="/login">login </Link>
-            <Link to="/register">register</Link>
-          </>
-        ) : (
-          <><Link to="/market">Market</Link>
-          <Link to="/newProduct">NewProduct</Link></>
-        )}
-      </div>
-    </>
-  );
-};
+
 
 export default App;
