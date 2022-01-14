6bd53cf45ac1d084e6a08fe7fc39555c85308e16
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-01-15 00:36:33 +0200

    add order/:id route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route,useParams} from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./context/auth";
 
@@ -9,10 +9,12 @@ import Login from "./components/Login"
 import Register from "./components/Rigester";
 import Market from "./components/Market";
 import NewProduct from "./components/NewProduct";
+import Cart from "./components/Cart";
+import Orders from "./components/Orders";
 import Order from "./components/Order";
 
 function App() {
-  const {isLogIn } = useContext(AuthContext);
+  const {isLogIn ,role} = useContext(AuthContext);
 
   return (
     <div className="App">
@@ -24,7 +26,10 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/market" element={isLogIn ? <Market /> : <></>} />
         <Route path="/newProduct" element={isLogIn ? <NewProduct /> : <></>} />
-        <Route path="/order" element={isLogIn ? <Order /> : <></>} />
+        <Route path="/cart" element={isLogIn && role =="user"? <Cart /> : <></>} />
+        <Route path="/order" element={isLogIn && role=='admin' ? <Orders /> : <></>} />
+        <Route path="/order/:id" element={isLogIn && role=='admin' ? <Order /> : <></>} />
+
 
       </Routes>
     </div>
